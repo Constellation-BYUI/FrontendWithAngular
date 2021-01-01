80de049a1fea6b3ee8f898bcb6b35afb3f68ca54
Nwokolo <nwokolobueze@gmail.com>
2020-12-31 17:50:27 -0700

    Created Components for Contellation App

---
+++ frontend/src/app/my-volt/my-volt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-volt',
+  templateUrl: './my-volt.component.html',
+  styleUrls: ['./my-volt.component.css']
+})
+export class MyVoltComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
